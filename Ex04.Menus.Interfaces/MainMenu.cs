99c3f404f9cd5a14b161ae635c41e4e780ba2413
ItaiKaplan <84511271+ItaiKaplan@users.noreply.github.com>
2021-05-31 20:36:18 +0300

    first change

---
+++ Ex04.Menus.Interfaces/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu : PressedObserver
+    {
+        public readonly List<MenuItem> m_MenuItems = new List<MenuItem>();
+        public MenuItem m_ExitMenuItem = new MenuItem();
+        private MenuItem m_CurrentMenuItem;
+
+        public MainMenu()
+        {
+            this.AddMenuItem(this.m_ExitMenuItem);
+        }
+        public void PreformTask(MenuItem i_MenuItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddMenuItem(MenuItem i_MenuItem)
+        {
+            i_MenuItem.AddListener(this as PressedObserver);
+            this.m_MenuItems.Add(i_MenuItem);
+        }
+
+        public void ItemClick(MenuItem i_MenuItem)
+        {
+            this.m_CurrentMenuItem = i_MenuItem;
+        }
+
+        public void ShowMenu()
+        {
+            while(this.m_CurrentMenuItem != this.m_ExitMenuItem)
+            {
+                if(this.m_CurrentMenuItem.isAction)
+                {
+                    RunMenuItemFunction(this.m_CurrentMenuitem);
+                }
+                DisplayMenuItem(this.m_CurrentMenuItem);
+            }
+        }
+    }
+}
---
+++ Ex04.Menus.Interfaces/MenuItem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuItem
+    {
+        private string m_Title;
+
+        public string Title
+        {
+            get
+            {
+                return m_Title;
+            }
+            set
+            {
+                m_Title = value;
+            }
+        }
+
+        private List<MenuItem> m_MenuItems;
+
+        public List<MenuItem> MenuItems
+        {
+            get
+            {
+                return m_MenuItems;
+            }
+            set
+            {
+                m_MenuItems = value;
+            }
+        }
+ 
+        private readonly List<PressedObserver> m_ClickListeners = new List<PressedObserver>();
+
+
+        public MenuItem()
+        {
+            //if(!this.IsAction)
+            //{
+            //    MenuItem Back = new MenuItem();
+            //    Back.AttachObserver(this as PressedObserver);
+            //    this.MenuItems.Add(Back);
+            //}
+        }
+        public void AddListener(PressedObserver i_PressedObserver)
+        {
+            m_ClickListeners.Add(i_PressedObserver);
+        }
+
+        public void RemoveListener(PressedObserver i_PressedObserver)
+        {
+            m_ClickListeners.Remove(i_PressedObserver);
+        }
+
+        OnClick()
+        {
+
+        }
+    }
+}
